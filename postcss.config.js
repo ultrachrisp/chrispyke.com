49e5926516ba6c258479e0859ed6659bd35e7f04
Chris Pyke <info@chrispyke.com>
2025-02-04 13:22:00 +0100

    build: got postCss mixins in play and working

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,7 @@
 module.exports = {
-  plugins: {
-    autoprefixer: {
-      browsers: ["last 2 versions"]
-    },
-    cssnano: {
-      preset: 'default',
-    },
-  },
+  plugins: [
+    require('postcss-mixins'),
+    require('autoprefixer'),
+    require('cssnano'),
+  ],
 };
